a1faef72de88511f3666f7d4c5f185924d698f56
Eunsub Lee <petlee@ucdavis.edu>
2018-08-15 16:51:28 -0700

    Display Name

---
+++ application.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/channels")
+def channels():
+    return render_template("channels.html")
